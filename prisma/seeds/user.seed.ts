bb7043ad0623792700056c71e8c02b485d2f5fe8
Reda Elemawy <redaelemawy98@gmail.com>
2024-09-01 16:10:42 +0300

    seeding completed successfully

--- prisma/seeds/user.seed.ts
+++ prisma/seeds/user.seed.ts
@@ -1,26 +1,37 @@
+const bcrypt = require("bcryptjs");
+
 const users = [
   {
-    id: 2,
-    name: "dada",
-    email: "redada@gmail.com",
-    password: "98s28283ndw",
-    roleId: 1,
-    username: "kdkdwwi",
+    id: 1,
+    name: "superadmin",
+    slug: "superadmin",
+    data: "{}",
+    leadership: false,
+  },
+];
+
+const cerdentials = [
+  {
+    id: 1,
+    username: "superadmin@mu.com",
+    password: bcrypt.hashSync("superadmin", 10),
+    userId: 1,
   },
 ];
 
 const roles = [
   {
     id: 1,
-    name: "admin",
+    name: "superadmin",
   },
-  { id: 2, name: "user" },
+  { id: 2, name: "admin" },
+  { id: 3, name: "user" },
 ];
 
 module.exports = async function seedFunction(db: any) {
   for (let role of roles) {
     await db.role.upsert({
-      where: { id: role.id },
+      where: { name: role.name },
       update: { ...role },
       create: {
         ...role,
@@ -29,11 +40,22 @@ module.exports = async function seedFunction(db: any) {
   }
 
   for (let user of users) {
+    let id = user.id;
+
     await db.user.upsert({
-      where: { id: user.id },
+      where: { id },
       update: { ...user },
       create: {
         ...user,
+      },
+    });
+  }
+  for (let cerdential of cerdentials) {
+    await db.cerdential.upsert({
+      where: { id: cerdential.id },
+      update: { ...cerdential },
+      create: {
+        ...cerdential,
       },
     });
   }
